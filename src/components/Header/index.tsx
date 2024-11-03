e0f17dda3feb33efc46ac8f014edf383e2796ce0
Baek <woonn97@gmail.com>
2024-11-04 02:36:46 +0900

    refactor: Header 컴포넌트로 나누기

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderWrapper, Logo } from "./style";
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Logo src="/logo.png" alt="logo" />
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  margin-bottom: 20px;
+`;
+
+export const Logo = styled.img`
+  width: 160px;
+`;
